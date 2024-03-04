20d0295a54be808cfd0379009d195bde7fe95458
Vero-Sampedro <verosampedro@icloud.com>
2024-03-04 18:20:54 +0100

    inializated repository

---
+++ src/main/java/dev/verosampedro/aniproject/AniprojectApplication.java
@@ -0,0 +1,13 @@
+package dev.verosampedro.aniproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AniprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AniprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/verosampedro/aniproject/AniprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.verosampedro.aniproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AniprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

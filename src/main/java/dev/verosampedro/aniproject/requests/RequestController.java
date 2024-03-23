3371884849a858600d26de31a1ca3f7b6e5823ca
Vero-Sampedro <verosampedro@icloud.com>
2024-03-23 18:06:33 +0100

    few changes

--- src/main/java/dev/verosampedro/aniproject/requests/RequestController.java
+++ src/main/java/dev/verosampedro/aniproject/requests/RequestController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,13 +38,13 @@ public class RequestController {
         return requestService.createRequest(request);
     }
 
-    @PostMapping("editRequest/{id}")
+    @PutMapping("/{id}")
     public Request updateRequest(@RequestBody Request request, @PathVariable Long id) {
         request.setId(id);
         return requestService.updateRequest(request);
     }
 
-    @DeleteMapping("deleteRequest/{id}")
+    @DeleteMapping("/{id}")
     public void deleteRequest(@PathVariable Long id) {
         requestService.deleteRequestById(id);
     }
